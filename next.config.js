d2781a11f569a75444a14ac9dd7160acaae71fcf
tenten1521 <134025719+tenten1521@users.noreply.github.com>
2023-06-27 23:56:12 +0800

    final

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images:{
+        domains: [
+            "orhnwvbstnxrbbcldsqb.supabase.co"
+        ]
+    }
+}
+
+module.exports = nextConfig
