f139c717536dc5e3b9809f11ee59d193090d6d17
IANNY002 <125583681+IANNY002@users.noreply.github.com>
2023-07-02 23:27:27 +0800

    Update PlayerContent.tsx
    ADDED REPEAT BUTTON

--- components/PlayerContent.tsx
+++ components/PlayerContent.tsx
@@ -1,8 +1,7 @@
 "use client";
-
 import useSound from "use-sound";
-import { useEffect, useState } from "react";
-import { BsPauseFill, BsPlayFill } from "react-icons/bs";
+import { useState, useEffect } from "react";
+import { BsPauseFill, BsPlayFill, BsArrowRepeat } from "react-icons/bs";
 import { HiSpeakerWave, HiSpeakerXMark } from "react-icons/hi2";
 import { AiFillStepBackward, AiFillStepForward } from "react-icons/ai";
 
@@ -19,13 +18,11 @@ interface PlayerContentProps {
   songUrl: string;
 }
 
-const PlayerContent: React.FC<PlayerContentProps> = ({ 
-  song, 
-  songUrl
-}) => {
+const PlayerContent: React.FC<PlayerContentProps> = ({ song, songUrl }) => {
   const player = usePlayer();
   const [volume, setVolume] = useState(1);
   const [isPlaying, setIsPlaying] = useState(false);
+  const [isRepeatEnabled, setIsRepeatEnabled] = useState(false);
 
   const Icon = isPlaying ? BsPauseFill : BsPlayFill;
   const VolumeIcon = volume === 0 ? HiSpeakerXMark : HiSpeakerWave;
@@ -43,7 +40,7 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
     }
 
     player.setId(nextSong);
-  }
+  };
 
   const onPlayPrevious = () => {
     if (player.ids.length === 0) {
@@ -58,28 +55,29 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
     }
 
     player.setId(previousSong);
-  }
+  };
 
-  const [play, { pause, sound }] = useSound(
-    songUrl,
-    { 
-      volume: volume,
-      onplay: () => setIsPlaying(true),
-      onend: () => {
-        setIsPlaying(false);
+  const [play, { pause, sound }] = useSound(songUrl, {
+    volume: volume,
+    onplay: () => setIsPlaying(true),
+    onend: () => {
+      setIsPlaying(false);
+      if (!isRepeatEnabled) {
         onPlayNext();
-      },
-      onpause: () => setIsPlaying(false),
-      format: ['mp3']
-    }
-  );
+      } else {
+        play(); // Play the same song again
+      }
+    },
+    onpause: () => setIsPlaying(false),
+    format: ["mp3"],
+  });
 
   useEffect(() => {
     sound?.play();
-    
+
     return () => {
       sound?.unload();
-    }
+    };
   }, [sound]);
 
   const handlePlay = () => {
@@ -88,7 +86,28 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
     } else {
       pause();
     }
-  }
+  };
+
+  const toggleRepeat = () => {
+    setIsRepeatEnabled(!isRepeatEnabled);
+  };
+
+  useEffect(() => {
+    let isMounted = true;
+
+    if (!sound) {
+      return;
+    } else {
+      sound.loop(isRepeatEnabled);
+    }
+
+    return () => {
+      if (isMounted) {
+        sound.loop(false);
+      }
+      isMounted = false;
+    };
+  }, [sound, isRepeatEnabled]);
 
   const toggleMute = () => {
     if (volume === 0) {
@@ -96,111 +115,117 @@ const PlayerContent: React.FC<PlayerContentProps> = ({
     } else {
       setVolume(0);
     }
-  }
+  };
 
   return ( 
     <div className="grid grid-cols-2 md:grid-cols-3 h-full">
-        <div className="flex w-full justify-start">
-          <div className="flex items-center gap-x-4">
-            <MediaItem data={song} />
-            <LikeButton songId={song.id} />
-          </div>
+      <div className="flex w-full justify-start">
+        <div className="flex items-center gap-x-4">
+          <MediaItem data={song} />
+          <LikeButton songId={song.id} />
         </div>
+      </div>
 
+      <div 
+        className="
+          flex 
+          md:hidden 
+          col-auto 
+          w-full 
+          justify-end 
+          items-center
+        "
+      >
         <div 
+          onClick={handlePlay} 
+          className="
+            h-10
+            w-10
+            flex 
+            items-center 
+            justify-center 
+            rounded-full 
+            bg-white 
+            p-1 
+            cursor-pointer
+          "
+        >
+          <Icon size={30} className="text-black" />
+        </div>
+      </div>
+
+      <div 
+        className="
+          hidden
+          h-full
+          md:flex 
+          justify-center 
+          items-center 
+          w-full 
+          max-w-[722px] 
+          gap-x-6
+        "
+      >
+        <AiFillStepBackward
+          onClick={onPlayPrevious}
+          size={30} 
+          className="
+            text-neutral-400 
+            cursor-pointer 
+            hover:text-white 
+            transition
+          "
+        />
+        <div 
+          onClick={handlePlay} 
           className="
             flex 
-            md:hidden 
-            col-auto 
-            w-full 
-            justify-end 
-            items-center
-          "
-        >
-          <div 
-            onClick={handlePlay} 
-            className="
-              h-10
-              w-10
-              flex 
-              items-center 
-              justify-center 
-              rounded-full 
-              bg-white 
-              p-1 
-              cursor-pointer
-            "
-          >
-            <Icon size={30} className="text-black" />
-          </div>
-        </div>
-
-        <div 
-          className="
-            hidden
-            h-full
-            md:flex 
-            justify-center 
             items-center 
-            w-full 
-            max-w-[722px] 
-            gap-x-6
+            justify-center
+            h-10
+            w-10 
+            rounded-full 
+            bg-white 
+            p-1 
+            cursor-pointer
           "
         >
-          <AiFillStepBackward
-            onClick={onPlayPrevious}
-            size={30} 
-            className="
-              text-neutral-400 
-              cursor-pointer 
-              hover:text-white 
-              transition
-            "
-          />
-          <div 
-            onClick={handlePlay} 
-            className="
-              flex 
-              items-center 
-              justify-center
-              h-10
-              w-10 
-              rounded-full 
-              bg-white 
-              p-1 
-              cursor-pointer
-            "
-          >
-            <Icon size={30} className="text-black" />
-          </div>
-          <AiFillStepForward
-            onClick={onPlayNext}
-            size={30} 
-            className="
-              text-neutral-400 
-              cursor-pointer 
-              hover:text-white 
-              transition
-            " 
-          />
+          <Icon size={30} className="text-black" />
         </div>
-
-        <div className="hidden md:flex w-full justify-end pr-2">
-          <div className="flex items-center gap-x-2 w-[120px]">
-            <VolumeIcon 
-              onClick={toggleMute} 
-              className="cursor-pointer" 
-              size={34} 
-            />
-            <Slider 
-              value={volume} 
-              onChange={(value) => setVolume(value)}
-            />
-          </div>
-        </div>
-
+        <AiFillStepForward
+          onClick={onPlayNext}
+          size={30} 
+          className="
+            text-neutral-400 
+            cursor-pointer 
+            hover:text-white 
+            transition
+          " 
+        />
+        <BsArrowRepeat
+          onClick={toggleRepeat}
+          className={`cursor-pointer ${isRepeatEnabled ? 'text-primary-500' : 'text-neutral-400'}`}
+          size={24}
+        />
       </div>
-   );
+
+      <div className="hidden md:flex w-full justify-end pr-2">
+        <div className="flex items-center gap-x-2 w-[120px]">
+          <VolumeIcon 
+            onClick={toggleMute} 
+            className="cursor-pointer" 
+            size={34} 
+          />
+          <Slider 
+            value={volume} 
+            onChange={(value) => setVolume(value)}
+          />
+        </div>
+      </div>
+
+      
+    </div>
+  );
 }
  
 export default PlayerContent;
