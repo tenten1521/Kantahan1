b8084668bd47b2ae51de8315f321089ed6c1d77f
IANNY002 <125583681+IANNY002@users.noreply.github.com>
2023-07-03 15:06:48 +0800

    Update Profilepage.tsx
    make the profile image changeable

--- app/account/component/Profilepage.tsx
+++ app/account/component/Profilepage.tsx
@@ -1,9 +1,10 @@
 "use client";
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { FaHeart, FaMusic, FaUserCircle } from 'react-icons/fa';
 
 const ProfilePage = () => {
   const [profileImage, setProfileImage] = useState<string | ArrayBuffer | null>(null);
+  const fileInputRef = useRef<HTMLInputElement>(null); // Reference to the file input field
 
   const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
@@ -26,32 +27,42 @@ const ProfilePage = () => {
     setIsHovered(!isHovered);
   };
 
+  const handleImageReset = () => {
+    setProfileImage(null);
+    if (fileInputRef.current) {
+      fileInputRef.current.value = ""; // Reset the value of the file input field
+    }
+  };
+
   return (
     <div className="flex flex-col items-start justify-center py-12 ml-4">
-        
       <div className="w-40 h-40 rounded-full overflow-hidden mb-4">
-        {profileImage ? (
-          <img src={profileImage as string} alt="Profile" className="w-full h-full object-cover" />
-        ) : (
-          <div
-            className={`flex items-center justify-center w-full h-full bg-gray-300 ${
-              isHovered ? 'hover:bg-gray-400' : ''
-            }`}
-            onMouseEnter={handleIconHover}
-            onMouseLeave={handleIconHover}
-          >
-            <label htmlFor="profile-image-upload" className="cursor-pointer">
-              <FaUserCircle className={`text-5xl text-white ${isHovered ? 'text-gray-600' : ''}`} />
-            </label>
-            <input
-              type="file"
-              id="profile-image-upload"
-              className="hidden"
-              accept="image/*"
-              onChange={handleImageUpload}
-            />
-          </div>
-        )}
+        <div
+          className={`flex items-center justify-center w-full h-full bg-gray-300 ${
+            isHovered ? 'hover:bg-gray-400' : ''
+          }`}
+          onMouseEnter={handleIconHover}
+          onMouseLeave={handleIconHover}
+          onClick={() => {
+            if (fileInputRef.current) {
+              fileInputRef.current.click(); // Trigger the file input field
+            }
+          }}
+        >
+          {profileImage ? (
+            <img src={profileImage as string} alt="Profile" className="w-full h-full object-cover" />
+          ) : (
+            <FaUserCircle className={`text-5xl text-white ${isHovered ? 'text-gray-600' : ''}`} />
+          )}
+        </div>
+        <input
+          type="file"
+          id="profile-image-upload"
+          className="hidden"
+          accept="image/*"
+          ref={fileInputRef}
+          onChange={handleImageUpload}
+        />
       </div>
       <h2 className="text-3xl font-semibold text-white mb-4">Lucius Artorius Castus</h2>
       <div className="flex flex-col items-center">
