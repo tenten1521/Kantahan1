c7286ccb51ddb61e9557886181d453d721c3ecc0
IANNY002 <125583681+IANNY002@users.noreply.github.com>
2023-07-03 17:47:30 +0800

    Update Profilepage.tsx
    designed

--- app/account/component/Profilepage.tsx
+++ app/account/component/Profilepage.tsx
@@ -6,6 +6,17 @@ const ProfilePage = () => {
   const [profileImage, setProfileImage] = useState<string | ArrayBuffer | null>(null);
   const fileInputRef = useRef<HTMLInputElement>(null); // Reference to the file input field
 
+  const [heartCount, setHeartCount] = useState(236);
+  const [musicCount, setMusicCount] = useState(512);
+
+  const incrementHeartCount = () => {
+    setHeartCount(heartCount + 1);
+  };
+
+  const incrementMusicCount = () => {
+    setMusicCount(musicCount + 1);
+  };
+
   const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     const reader = new FileReader();
@@ -35,8 +46,8 @@ const ProfilePage = () => {
   };
 
   return (
-    <div className="flex flex-col items-start justify-center py-12 ml-4">
-      <div className="w-40 h-40 rounded-full overflow-hidden mb-4">
+    <div className="flex flex-col items-start justify-start h-fit bg-gradient-to-b from-pink-800 p-6 py-12 ml-4 rounded-xl">
+      <div className="w-80 h-80 rounded-full overflow-hidden mb-4">
         <div
           className={`flex items-center justify-center w-full h-full bg-gray-300 ${
             isHovered ? 'hover:bg-gray-400' : ''
@@ -65,7 +76,9 @@ const ProfilePage = () => {
           ) : (
             <FaUserCircle className={`text-5xl text-white ${isHovered ? 'text-gray-600' : ''}`} />
           )}
+          
         </div>
+        
         <input
           type="file"
           id="profile-image-upload"
@@ -76,18 +89,29 @@ const ProfilePage = () => {
         />
       </div>
       <h2 className="text-3xl font-semibold text-white mb-4">Lucius Artorius Castus</h2>
-      <div className="flex flex-col items-center">
-        <div className="flex flex-col items-start gap-4 mb-8">
-          <div className="flex items-center gap-1 text-white">
-            <FaHeart />
-            <span className="text-xl">236</span>
-          </div>
-          <div className="flex items-center gap-1 text-white">
-            <FaMusic />
-            <span className="text-xl">512</span>
-          </div>
+      <div className="flex flex-row items-center">
+      <div className="flex flex-col items-center gap-4 mb-8">
+        <div className={`flex items-center gap-1 text-white ${isHovered ? 'cursor-pointer' : ''}`} onClick={incrementHeartCount}>
+          <button
+                  type="button"
+                  className="text-white bg-black bg-opacity-40 rounded-full p-2 hover:bg-opacity-60"
+                >
+                  <FaHeart />
+                </button>
+          <span className="text-xl">{heartCount}</span>
+        </div>
+        <div className={`flex items-center gap-1 text-white ${isHovered ? 'cursor-pointer' : ''}`} onClick={incrementMusicCount}>
+          <button
+                  type="button"
+                  className="text-white bg-black bg-opacity-40 rounded-full p-2 hover:bg-opacity-60"
+                >
+                  <FaMusic />
+                </button>
+          <span className="text-xl">{musicCount}</span>
         </div>
       </div>
+    </div>
+
     </div>
   );
 };
